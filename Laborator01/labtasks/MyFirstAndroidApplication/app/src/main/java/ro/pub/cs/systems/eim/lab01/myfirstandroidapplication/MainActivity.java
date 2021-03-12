f0fc1455157656f053660c4862d26575c9a37bcc
Teodor Popescu <teodor.popescu99@gmail.com>
2021-03-12 14:33:40 +0200

    rezolvare laborator01

--- Laborator01/labtasks/MyFirstAndroidApplication/app/src/main/java/ro/pub/cs/systems/eim/lab01/myfirstandroidapplication/MainActivity.java
+++ Laborator01/labtasks/MyFirstAndroidApplication/app/src/main/java/ro/pub/cs/systems/eim/lab01/myfirstandroidapplication/MainActivity.java
@@ -3,6 +3,7 @@ package ro.pub.cs.systems.eim.lab01.myfirstandroidapplication;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -22,8 +23,13 @@ public class MainActivity extends AppCompatActivity {
             greetingTextView.setAlpha(1);
 
             // TODO: exercise 9a
+            greetingTextView.setText(greetingTextView.getText().toString().replace("xxx", userNameEditText.getText().toString()));
 
             // TODO: exercise 9b
+            AlphaAnimation fadeAnimation = new AlphaAnimation(1.0f, 0.0f);
+            fadeAnimation.setDuration(TRANSPARENCY_EFFECT_DURATION);
+            fadeAnimation.setFillAfter(true);
+            greetingTextView.setAnimation(fadeAnimation);
         }
 
     }
